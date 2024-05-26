5614c106ea211395635c5089acfb254722ca2327
divyank22 <divyankpandey37@gmail.com>
2024-05-27 00:10:03 +0530

    started
    Vue js and logic of title and description

---
+++ src/App.vue
@@ -0,0 +1,6 @@
+<script setup>
+import todoapp from './components/todoapp.vue';
+</script>
+<template>
+  <todoapp/>
+</template>
---
+++ src/components/todoapp.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from 'vue'
+const title = ref('')
+const description = ref('')
+const todos = ref([]) // Initialize todos as an empty array
+const addtodo = () => {
+  if (title.value.trim() !== '' && description.value.trim() !== '') {
+    todos.value.push({ title: title.value, description: description.value })
+    title.value = ''
+    description.value = ''
+  }
+}
+const removetask = (index) => {
+  todos.value.splice(index, 1)
+}
+</script>
+
+<template>
+  <div class="todo-app">
+    <div class="task-input">
+      <input v-model="title" @keyup.enter="addtodo" placeholder="Add new task" />
+      <input v-model="description" @keyup.enter="addtodo" placeholder="Add new description" />
+      <button @click="addtodo">Add Todo</button>
+    </div>
+    <ul>
+      <li v-for="(todo, i) in todos" :key="i">
+        {{ todo.title }}
+        <p>{{ todo.description }}</p>
+        <button @click="removetask(i)">Remove</button>
+      </li>
+    </ul>
+  </div>
+</template>
